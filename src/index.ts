3a46166c64934cd2c424360e524cf25e3abb865c
Rongjian Zhang <pd4d10@gmail.com>
2022-04-03 00:16:41 +0800

    refactor: use unplugin

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,15 @@
-import fs from "node:fs";
-import { Plugin } from "esbuild";
+import { createUnplugin } from "unplugin";
 
-export default function propertiesPlugin(): Plugin {
+export default createUnplugin(() => {
   return {
-    name: "properties-plugin",
-    setup: (build) => {
-      build.onLoad(
-        { filter: /\.properties$/, namespace: "file" },
-        async (args) => {
-          const content = await fs.promises.readFile(args.path, "utf-8");
-          // @ts-ignore
-          const { parse } = await import("properties");
-
-          return {
-            contents: JSON.stringify(parse(content)),
-            loader: "json",
-          };
-        }
-      );
+    name: "unplugin-properties",
+    transformInclude(id) {
+      return id.endsWith(".properties");
+    },
+    async transform(code) {
+      // @ts-ignore
+      const { parse } = await import("properties");
+      return "export default " + JSON.stringify(parse(code));
     },
   };
-}
+});
